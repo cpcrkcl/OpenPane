d8579417f3f4679337ba41d96ea91b892acd6da8
cpcr.kcl <cpcr.kcl@gmail.com>
2026-06-04 21:19:20 -0400

    chore:initial macOS app

---
+++ OpenPane/OpenPaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenPaneApp.swift
+//  OpenPane
+//
+//  Created by Christopher Rego on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenPaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenPaneTests/OpenPaneTests.swift
@@ -0,0 +1,19 @@
+//
+//  OpenPaneTests.swift
+//  OpenPaneTests
+//
+//  Created by Christopher Rego on 6/4/26.
+//
+
+import Testing
+@testable import OpenPane
+
+struct OpenPaneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
